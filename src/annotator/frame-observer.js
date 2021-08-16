985e5dcddcf1b5a264d6d2ea8504c149f4f7b97e
Eduardo Sanz García <esanzgar@gmail.com>
2021-08-16 17:22:50 +0200

    Improve the logic that checks for iframe's document ready
    In certain situations, the logic to check when the iframe's document is
    ready didn't work as expected. The reason for that is that browsers
    create an initial blank document before the actual document loads.
    This blank document has a `readyState` of 'complete' in Safari and
    Chrome and of 'uninitialized' in Firefox. This blank document can be
    mistaken by the final document. I was able to reproduce a situation
    where the Hypothesis client was injected into the blank document. Then,
    the blank document was replaced by the actual document and the
    Hypothesis client was not not longer embedded.
    
    In addition, this commit:
    
    * renames `frame` to `iframe`
    * renames `_handledFrames` to _annotatableIFrames`
    * removes `isAccessible` (no longer neede)
    * enables a test that was commented previously

--- src/annotator/frame-observer.js
+++ src/annotator/frame-observer.js
@@ -26,7 +26,7 @@ export class FrameObserver {
     this._onFrameAdded = onFrameAdded;
     this._onFrameRemoved = onFrameRemoved;
     /** @type {Set<HTMLIFrameElement>} */
-    this._handledFrames = new Set();
+    this._annotatableIFrames = new Set();
     this._isDisconnected = false;
 
     this._mutationObserver = new MutationObserver(
@@ -48,31 +48,35 @@ export class FrameObserver {
   }
 
   /**
-   * @param {HTMLIFrameElement} frame
+   * @param {HTMLIFrameElement} iframe
    */
-  async _addFrame(frame) {
-    this._handledFrames.add(frame);
-    if (isAccessible(frame)) {
-      await onDocumentReady(frame);
+  async _addFrame(iframe) {
+    this._annotatableIFrames.add(iframe);
+    try {
+      await onDocumentReady(iframe);
       if (this._isDisconnected) {
         return;
       }
-      const frameWindow = /** @type {Window} */ (frame.contentWindow);
-      frameWindow.addEventListener('unload', () => {
-        this._removeFrame(frame);
+      const iframeWindow = iframe.contentWindow;
+      // @ts-expect-error
+      // This line raises an exception if the iframe is from a different origin
+      iframeWindow.addEventListener('unload', () => {
+        this._removeFrame(iframe);
       });
-      this._onFrameAdded(frame);
-    } else {
-      // Could warn here that frame was not cross origin accessible
+      this._onFrameAdded(iframe);
+    } catch (e) {
+      console.warn(
+        `Unable to inject the Hypothesis client (from '${document.location}' into a cross-origin iframe '${iframe.src}')`
+      );
     }
   }
 
   /**
-   * @param {HTMLIFrameElement} frame
+   * @param {HTMLIFrameElement} iframe
    */
-  _removeFrame(frame) {
-    this._handledFrames.delete(frame);
-    this._onFrameRemoved(frame);
+  _removeFrame(iframe) {
+    this._annotatableIFrames.delete(iframe);
+    this._onFrameRemoved(iframe);
   }
 
   _discoverFrames() {
@@ -83,13 +87,15 @@ export class FrameObserver {
     );
 
     for (let frame of frames) {
-      if (!this._handledFrames.has(frame)) {
+      if (!this._annotatableIFrames.has(frame)) {
+        this._annotatableIFrames.add(frame);
         this._addFrame(frame);
       }
     }
 
-    for (let frame of this._handledFrames) {
+    for (let frame of this._annotatableIFrames) {
       if (!frames.has(frame)) {
+        this._annotatableIFrames.delete(frame);
         this._removeFrame(frame);
       }
     }
@@ -97,33 +103,48 @@ export class FrameObserver {
 }
 
 /**
- * Check if we can access this iframe's document
- *
- * @param {HTMLIFrameElement} iframe
- */
-function isAccessible(iframe) {
-  try {
-    return !!iframe.contentDocument;
-  } catch (e) {
-    return false;
-  }
-}
-
-/**
- * Resolves a Promise when the iframe's DOM is ready (loaded and parsed)
+ * Resolves a Promise when the iframe's document is ready (loaded and parsed)
  *
  * @param {HTMLIFrameElement} iframe
  * @return {Promise<void>}
+ * @throws {Error} if trying to access a document from a cross-origin iframe
  */
 export function onDocumentReady(iframe) {
   return new Promise(resolve => {
-    const iframeDocument = /** @type {Document} */ (iframe.contentDocument);
-    if (iframeDocument.readyState === 'loading') {
-      iframeDocument.addEventListener('DOMContentLoaded', () => {
+    // @ts-expect-error
+    const iframeDocument = iframe.contentWindow.document;
+    const state = iframeDocument.readyState;
+
+    // Web browsers initially load a blank document before the final document.
+    // This blank document is (1) accessible, (2) has a 'complete' readyState,
+    // on Chrome and Safari, and an 'uninitialized' readyState on Firefox and
+    // (3) has a empty body and head. If a blank document is detected and there
+    // is a 'src' attribute, it is expected that the blank document will be
+    // replaced by the document from 'src'.
+    /* istanbul ignore next */
+    if (
+      // @ts-ignore
+      state === 'uninitialized' ||
+      (state === 'complete' &&
+        iframe.hasAttribute('src') &&
+        iframeDocument.head.hasChildNodes() === false &&
+        iframeDocument.body.hasChildNodes() === false)
+    ) {
+      // Unfortunately, listening for 'DOMContentLoaded' on the iframeDocument
+      // doesn't work. Instead, we need to wait for a 'load' event to be triggered.
+      iframe.addEventListener('load', () => {
         resolve();
       });
-    } else {
-      resolve();
+      return;
     }
+
+    /* istanbul ignore next */
+    if (state === 'loading') {
+      iframeDocument.addEventListener('DOMContentLoaded', () => resolve());
+      return;
+    }
+
+    // state is 'interactive' or 'complete';
+    resolve();
   });
 }
