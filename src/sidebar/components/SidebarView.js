6df0061be6f43edfba6126a9a7f431897563776d
vrde <agranzot@mailbox.org>
2021-10-22 17:09:11 +0200

    Disable tabs when the rewrite is open

--- src/sidebar/components/SidebarView.js
+++ src/sidebar/components/SidebarView.js
@@ -223,7 +223,7 @@ function SidebarView({
       {hasDirectLinkedGroupError && (
         <SidebarContentError errorType="group" onLoginRequest={onLogin} />
       )}
-      {showTabs && <SelectionTabs isLoading={isLoading} />}
+      {!enableTheRewrite && showTabs && <SelectionTabs isLoading={isLoading} />}
       <label>
         The Rewrite{' '}
         <input
